bc94d56b927db4ad4320e1b83eebc3c4e2cb87d0
Dev-Faith <adebayofaith0@gmail.com>
2025-04-13 20:45:53 +0100

    Fixed the type error for Team page

--- app/dashboard/team/page.tsx
+++ app/dashboard/team/page.tsx
@@ -1,92 +1,80 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-interface TeamMember {
-  id: string;
-  name: string;
-  role: string;
-  avatar: string;
-}
+const heading = "Meet our team";
+const subheading = "Group 8 Team Members";
+const description =
+  "This is the team that worked on the project. We combined efforts in different areas to achieve the best results. We are a team of engineers, designers, and developers who are passionate about technology and innovation.";
 
-interface Team1Props {
-  heading: string;
-  subheading: string;
-  description: string;
-  members: TeamMember[];
-}
+const members = [
+  {
+    id: "person-1",
+    name: "Chris",
+    role: "Control Systems Design and Analysis",
+    avatar: "https://shadcnblocks.com/images/block/avatar-1.webp",
+  },
+  {
+    id: "person-2",
+    name: "Barnabas",
+    role: "Software Design || documentation",
+    avatar: "https://shadcnblocks.com/images/block/avatar-2.webp",
+  },
+  {
+    id: "person-3",
+    name: "Faith",
+    role: "⁠IoT App and Web Interface Design",
+    avatar: "https://shadcnblocks.com/images/block/avatar-3.webp",
+  },
+  {
+    id: "person-4",
+    name: "Abdulrauf",
+    role: "Control Systems Design and Analysis ",
+    avatar: "https://shadcnblocks.com/images/block/avatar-4.webp",
+  },
+  {
+    id: "person-5",
+    name: "Alfred",
+    role: "Hardware Design and Analysis",
+    avatar: "https://shadcnblocks.com/images/block/avatar-5.webp",
+  },
+  {
+    id: "person-6",
+    name: "Nadim",
+    role: "Documentation and Slides",
+    avatar: "https://shadcnblocks.com/images/block/avatar-6.webp",
+  },
+  {
+    id: "person-7",
+    name: "Blessed",
+    role: "Hardware Design and Analysis",
+    avatar: "https://shadcnblocks.com/images/block/avatar-7.webp",
+  },
+  {
+    id: "person-8",
+    name: "Marcel",
+    role: "Hardware Design and Analysis",
+    avatar: "https://shadcnblocks.com/images/block/avatar-8.webp",
+  },
+  {
+    id: "person-9",
+    name: "Alibaba",
+    role: "Hardware Design and Analysis",
+    avatar: "https://shadcnblocks.com/images/block/avatar-8.webp",
+  },
+  {
+    id: "person-10",
+    name: "Hassan",
+    role: "IoT App and Web Interface Design",
+    avatar: "https://shadcnblocks.com/images/block/avatar-8.webp",
+  },
+  {
+    id: "person-11",
+    name: "Tahiru",
+    role: "IoT App and Web Interface Design",
+    avatar: "https://shadcnblocks.com/images/block/avatar-8.webp",
+  },
+];
 
-const Page = ({
-  heading = "Meet our team",
-  subheading = "Group 8 Team Members",
-  description = "This is the team that worked on the project. We combined efforts in different areas to achieve the best results. We are a team of engineers, designers, and developers who are passionate about technology and innovation.",
-  members = [
-    {
-      id: "person-1",
-      name: "Chris",
-      role: "Control Systems Design and Analysis",
-      avatar: "https://shadcnblocks.com/images/block/avatar-1.webp",
-    },
-    {
-      id: "person-2",
-      name: "Barnabas",
-      role: "Software Design || documentation",
-      avatar: "https://shadcnblocks.com/images/block/avatar-2.webp",
-    },
-    {
-      id: "person-3",
-      name: "Faith",
-      role: "⁠IoT App and Web Interface Design",
-      avatar: "https://shadcnblocks.com/images/block/avatar-3.webp",
-    },
-    {
-      id: "person-4",
-      name: "Abdulrauf",
-      role: "Control Systems Design and Analysis ",
-      avatar: "https://shadcnblocks.com/images/block/avatar-4.webp",
-    },
-    {
-      id: "person-5",
-      name: "Alfred",
-      role: "Hardware Design and Analysis",
-      avatar: "https://shadcnblocks.com/images/block/avatar-5.webp",
-    },
-    {
-      id: "person-6",
-      name: "Nadim",
-      role: "Documentation and Slides",
-      avatar: "https://shadcnblocks.com/images/block/avatar-6.webp",
-    },
-    {
-      id: "person-7",
-      name: "Blessed",
-      role: "Hardware Design and Analysis",
-      avatar: "https://shadcnblocks.com/images/block/avatar-7.webp",
-    },
-    {
-      id: "person-8",
-      name: "Marcel",
-      role: "Hardware Design and Analysis",
-      avatar: "https://shadcnblocks.com/images/block/avatar-8.webp",
-    },
-    {
-      id: "person-9",
-      name: "Alibaba",
-      role: "Hardware Design and Analysis",
-      avatar: "https://shadcnblocks.com/images/block/avatar-8.webp",
-    },
-    {
-      id: "person-10",
-      name: "Hassan",
-      role: "IoT App and Web Interface Design",
-      avatar: "https://shadcnblocks.com/images/block/avatar-8.webp",
-    },
-    {
-      id: "person-11",
-      name: "Tahiru",
-      role: "IoT App and Web Interface Design",
-      avatar: "https://shadcnblocks.com/images/block/avatar-8.webp",
-    },
-  ],
-}: Team1Props) => {
+const Page = () => {
   return (
     <section className="py-32">
       <div className="container flex flex-col items-center text-center">
